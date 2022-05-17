5605bef877dc0ee47f44e482f26c404bd69213db
jiangfan <fanjiang@qq.com>
2022-05-17 16:24:18 +0800

    第一次提交

---
+++ git-demo/src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * @author ：jiangfan
+ * @date ：2022/5/17 16:22
+ * @description：
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("git 1");
+    }
+}
